069832a70e27aa3a69d48371cb7f62e0fd4daebe
Thiago Sousa <thiagohasousa@outlook.com>
2024-02-23 14:34:08 -0300

    Segundo commit

---
+++ 1044.cs
@@ -0,0 +1,12 @@
+﻿namespace ProjetosBeecrowd
+{
+    internal class _1044
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Teste de comitt");
+            Console.ReadLine();
+        }
+    }
+}
