34844639ed7c1c8f5f00a551c0e0a882d7da9ede
Thiago Sousa <thiagohasousa@outlook.com>
2024-03-01 21:31:53 -0300

    Teste

--- 1044.cs
+++ 1044.cs
@@ -1,13 +1,27 @@
-﻿namespace ProjetosBeecrowd
+﻿using System;
+
+namespace ProjetosBeecrowd
 {
     internal class _1044
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Teste de comitt");
-            Console.WriteLine("Sera que foi");
-            Console.ReadLine();
+            int A, B;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            A = int.Parse(vet[0]);
+            B = int.Parse(vet[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+
         }
     }
 }
