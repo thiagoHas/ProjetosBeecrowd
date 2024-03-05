3c7cabadbadad4aabe7585ce2cd5c5cbceb017ff
Thiago Sousa <thiagohasousa@outlook.com>
2024-03-05 20:12:57 -0300

    Mais um

---
+++ 1046.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ProjetosBeecrowd
+{
+    internal class _1046
+    {
+        static void Main(string[] args)
+        {
+
+            int horaInicial, horaFinal;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            horaInicial = int.Parse(vet[0]);
+            horaFinal = int.Parse(vet[1]);
+
+            int duracao;
+            if(horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU "+duracao+" HORA(S)");
+
+        }
+    }
+}
